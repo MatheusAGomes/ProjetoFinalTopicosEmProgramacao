ca18216cff082f3ccd9d238b5b1ca11e80752907
MatheusAGomes <matheusgoassuncao@gmail.com>
2022-12-07 23:04:18 -0300

    api consome mas nao mostra

--- ProjetoTopicosEmProgramacao/routes/admin.js
+++ ProjetoTopicosEmProgramacao/routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express();
 const Loaders = require('../startdb.js')
-const UserModel = require('../Models/userModel.js')
+const UserModel = require('../Models/userModel.js');
+
+const { default: axios } = require('axios');
 router.set('view engine','ejs')
 
+//"https://www.googleapis.com/books/v1/volumes?q=HarryPotter&key=AIzaSyB0tE_alkPXEnuRdhd3PtaUwiFFEISIsSI";
 let emails;
 let tamanho_do_array;
 let objeto_do_usuario;
@@ -13,14 +16,26 @@ Loaders.start();
 router.get('/', async(req, res, next) => {
   res.render(__dirname+'/views/index.ejs')
   emails = await UserModel.find();
-  console.log(emails)
-  console.log(emails.length)
   tamanho_do_array = emails.length;
 });
 /*
 router.get('/css', (req, res, next) => {
   res.sendFile()
 });
+*/
+
+
+
+
+/*
+
+
+
+LOGIN
+
+
+
+
 */
 router.post('/', (req, res, next) => {
 
@@ -37,6 +52,19 @@ router.post('/', (req, res, next) => {
   
   
 });
+
+
+
+
+/*
+
+
+
+NovoUsuario
+
+
+
+*/
 
 router.get('/NovoUsuario',  (req, res, next) => {
   res.sendFile(__dirname+'/views/CreateCount.html');
@@ -61,21 +89,89 @@ router.post('/NovoUsuario', async (req, res, next) => {
         }
         const createCount =  await UserModel.create(objeto)
         res.redirect('/');
-        
-
 });
+
+
+
+
+/*
+
+
+
+DASHBORD
+
+
+
+
+*/
+
+
+
+
 
 
 
 router.get('/DashBord/:id', async(req, res, next) => {
   let valordoid = req.params.id
   let teste;
-  console.log(valordoid)
   objetodousuario =  await UserModel.find({_id:'639113566d8e6ef04676690c'})
- console.log(objetodousuario)
   res.render(__dirname+'/views/DashBord.ejs',{NomeDoUsuario:objetodousuario[0].name})
 });
 
+
+
+
+
+
+/*
+
+
+
+MINHA PESQUISA
+
+
+
+
+*/
+
+router.get('/MinhaPesquisa/:id', async(req, res, next) => {
+
+  let valordoid = req.params.id
+  let teste;
+  objetodousuario =  await UserModel.find({_id:'639113566d8e6ef04676690c'})
+  res.render(__dirname+'/views/MinhaPesquisa.ejs',{NomeDoUsuario:objetodousuario[0].name})
+});
+router.post('/MinhaPesquisa/:id', async(req, res, next) => {
+  let valordoid = req.params.id
+  let teste;
+  objetodousuario =  await UserModel.find({_id:'639113566d8e6ef04676690c'})
+
+  let nomeDoLivro = req.body.NomeDoLivro
+  console.log(nomeDoLivro)
+
+  let apigoogleBook = `https://www.googleapis.com/books/v1/volumes?q=${nomeDoLivro}&key=AIzaSyB0tE_alkPXEnuRdhd3PtaUwiFFEISIsSI`
+  
+  //console.log(apigoogleBook)
+
+  let arrayDeResposta = await axios.get(apigoogleBook);
+  //console.log(arrayDeResposta.data.items[0])
+
+  let objetoLivro = {
+    Nome:arrayDeResposta.data.items[0].volumeInfo.title,
+    autor:arrayDeResposta.data.items[0].volumeInfo.authors[0],
+    description:arrayDeResposta.data.items[0].volumeInfo.description,
+    quantidadedePaginas:arrayDeResposta.data.items[0].volumeInfo.pageCount,
+  }
+
+  res.redirect(`/MinhaPesquisaComResposta/${valordoid}/${objetoLivro.Nome}/${objetoLivro.autor}/${objetoLivro.description}/${objetoLivro.quantidadedePaginas}/`);
+
+  //res.render(__dirname+`/views/MinhaPesquisaComResultados.ejs`,{NomeDoLivro:objetoLivro.Nome,Autor:objetoLivro.autor,description:objetoLivro.description,quantidadedePaginas:objetoLivro.quantidadedePaginas})
+});
+
+
+router.post('/MinhaPesquisaComResposta/:id?/:nome?/:autor?/:description/:quantidadedePaginas?', async(req, res, next) => {
+console.log(objetoLivro)
+});
 
 
 
